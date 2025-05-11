6985f4863e234bc98ff41de0ece0613d52c612f3
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-11 15:31:03 +0100

    Finished beer advisor app from chapter 2
    Added an OnClickListener to the button, which polls the spinner's current
    value, uses that value to get a corresponding list, and then reduces
    that list to a single string and assigns that to the TextView.

--- beer_advisor/app/src/main/java/com/studio/beeradvisor/MainActivity.kt
+++ beer_advisor/app/src/main/java/com/studio/beeradvisor/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.studio.beeradvisor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.find_beer).setOnClickListener {
+            val beers = getBeers(findViewById<Spinner>(R.id.beer_colour).selectedItem.toString())
+            findViewById<TextView>(R.id.brands).text = beers.reduce{str, item -> str + '\n' + item}
+        }
+    }
+
+    private fun getBeers(colour: String): List<String> {
+        return when (colour) {
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            "Dark" -> listOf("Gout Stout", "Dark Daniel")
+            else -> listOf("Unknown beer colour $colour selected; please contact your system administrator.")
+        }
     }
 }
