6db8f5b30a4ee9a12406cbdaaa4442eea0e7fe7b
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-15 17:58:16 +0100

    Adds onSaveInstanceStae
    Exploring the activity lifecycle and the most basic way Android can
    persist variables across instances of an activity.

--- stopwatch/app/src/main/java/com/studio/stopwatch/MainActivity.kt
+++ stopwatch/app/src/main/java/com/studio/stopwatch/MainActivity.kt
@@ -12,12 +12,26 @@ class MainActivity : AppCompatActivity() {
     var running = false
     var offset: Long = 0
 
+    val OFFSET_KEY = "offset"
+    val RUNNING_KEY = "running"
+    val BASE_KEY = "base"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         stopwatch = findViewById<Chronometer>(R.id.stopwatch)
+
+        if (savedInstanceState != null) {
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+
+            if (running) {
+                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                stopwatch.start()
+            } else setBaseTime()
+        }
 
         findViewById<Button>(R.id.start_button).setOnClickListener {
             if (!running) {
@@ -41,11 +55,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun setBaseTime() {
-        stopwatch.base = SystemClock.elapsedRealtime() - offset
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(BASE_KEY, stopwatch.base)
+        super.onSaveInstanceState(outState)
     }
 
-    fun saveOffset() {
-        offset = SystemClock.elapsedRealtime() - stopwatch.base
-    }
+    fun setBaseTime() { stopwatch.base = SystemClock.elapsedRealtime() - offset }
+
+    fun saveOffset() { offset = SystemClock.elapsedRealtime() - stopwatch.base }
 }
