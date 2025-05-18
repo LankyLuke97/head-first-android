b603b73f4f4e0e30dbd706a7e1f434589587fb48
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-18 14:36:09 +0100

    Using the nav host to navigate between fragments
    The book I am following instructs you to attach the
    navController.navigate call to the button within onCreateView in the
    fragment. However, this does not appear to work. After much rooting
    around documents, it appears to be that the view has not attached to the
    navHostFragment at this point, and so the call to view.findNavController
    fails.
    
    There are two options that I have found to fix this. The first is to use
    requireActivity.findNavController(R.id.nav_host_fragment) to explictly
    call the activity_main.xml layout for its nav controller. The second,
    which I have gone for, is to call view.findNavController() within
    onViewCreated, which is called once the view has finished creating and
    attached to the root fragmentContainer.

--- secret_message/app/src/main/java/com/studio/secretmessage/MainActivity.kt
+++ secret_message/app/src/main/java/com/studio/secretmessage/MainActivity.kt
@@ -2,11 +2,11 @@ package com.studio.secretmessage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
--- secret_message/app/src/main/java/com/studio/secretmessage/WelcomeFragment.kt
+++ secret_message/app/src/main/java/com/studio/secretmessage/WelcomeFragment.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = view.findNavController()
+
+        view.findViewById<Button>(R.id.start).setOnClickListener {
+            navController.navigate(R.id.action_welcomeFragment_to_messageFragment)
+        }
+    }
+
 }
