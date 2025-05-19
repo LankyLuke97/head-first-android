9e66677a8100edf5963b29747e3e28b080afd789
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-19 17:50:08 +0100

    Adds encryptFragment to the navigation process

--- secret_message/app/src/main/java/com/studio/secretmessage/MessageFragment.kt
+++ secret_message/app/src/main/java/com/studio/secretmessage/MessageFragment.kt
@@ -5,9 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class MessageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_message, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = view.findNavController()
+
+        view.findViewById<Button>(R.id.next).setOnClickListener {
+            navController.navigate(R.id.action_messageFragment_to_encryptFragment)
+        }
     }
 }
