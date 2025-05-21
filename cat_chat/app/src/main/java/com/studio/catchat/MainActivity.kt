c00f8a0c5a4bf88b8b3a05dfcbf860cb190187eb
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-21 19:36:08 +0100

    Creates CatChat app with non-default toolbar

---
+++ cat_chat/app/src/main/java/com/studio/catchat/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.studio.catchat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+}
