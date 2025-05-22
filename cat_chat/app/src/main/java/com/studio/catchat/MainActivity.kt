0a142d6136be7ca94092af4cb39c878b2fd8dbf6
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-22 08:03:23 +0100

    Adds nav_graph based functionality to the custom toolbar.
    Additionally, starting to figure out how the gradle build stuff works a
    bit better. There appears to be a libs.toml.versions file under the
    gradle folder at the project level which is the new way of managing
    versions and whatnot for dependencies; this is also where the details
    are going when I switch from implementation("some.sort.of.string") to
    implementation(libs.string.reference).

--- cat_chat/app/src/main/java/com/studio/catchat/MainActivity.kt
+++ cat_chat/app/src/main/java/com/studio/catchat/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.studio.catchat
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.appbar.MaterialToolbar
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
