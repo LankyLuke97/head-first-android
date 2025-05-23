dc3c1f76f014958c35a1b823ee24b9d923c5faff
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-23 07:25:42 +0100

    Adds a bottom navigaton bar
    Much simpler than hooking up a toolbar to navigate, this bottom bar
    simply links to some menu items that you configure to have IDs matching
    those of fragments, and then you call .setupWithNavController on the
    bottom bar; everything else sorts itself.

--- cat_chat/app/src/main/java/com/studio/catchat/SentItemsFragment.kt
+++ cat_chat/app/src/main/java/com/studio/catchat/SentItemsFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 class SentItemsFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         return inflater.inflate(R.layout.fragment_sent_items, container, false)
     }
 }
