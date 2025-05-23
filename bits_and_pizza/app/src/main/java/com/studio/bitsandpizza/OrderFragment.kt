9be7a1182952b89858fe0a59752220d4daff1087
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-23 16:25:21 +0100

    Finishes pizza app
    More complex app and views, using scrollable and resizing toolbars, a
    floating action button, radio group, chip group, toasts, and snackbars.

--- bits_and_pizza/app/src/main/java/com/studio/bitsandpizza/OrderFragment.kt
+++ bits_and_pizza/app/src/main/java/com/studio/bitsandpizza/OrderFragment.kt
@@ -5,14 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.chip.Chip
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class OrderFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
         val view = inflater.inflate(R.layout.fragment_order, container, false)
         val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = pizzaGroup.checkedRadioButtonId
+            if (pizzaType == -1) {
+                val text = R.string.choose_pizza_toast
+                Toast.makeText(activity, text, Toast.LENGTH_LONG).show()
+            } else {
+                var text = (when (pizzaType) {
+                    R.id.radio_diavolo -> getString(R.string.diavolo)
+                    R.id.radio_funghi -> getString(R.string.funghi)
+                    else -> "Unknown"
+                }) + " pizza"
+                val chilli = view.findViewById<Chip>(R.id.chill_oil)
+                val parmesan = view.findViewById<Chip>(R.id.parmesan)
+
+                text += if (parmesan.isChecked) ", extra parmesan" else ""
+                text += if (chilli.isChecked) ", extra chilli oil" else ""
+                Snackbar.make(fab, text, Snackbar.LENGTH_LONG).show()
+            }
+        }
+
         return view
     }
 }
