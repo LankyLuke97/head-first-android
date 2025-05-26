cc0ee46cb77fbe19a8ddcee6544a4dd9e194e446
LankyLuke97 <lukehowden@rocketmail.com>
2025-05-26 09:06:01 +0100

    Adds first version of the guessing game
    Using tools weve already explored to build a guessing game that will
    allow the exploration of view models in place of Bundles for saving and
    managing state and logic.

---
+++ guessing_game/app/src/main/java/com/studio/guessinggame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.studio.guessinggame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
